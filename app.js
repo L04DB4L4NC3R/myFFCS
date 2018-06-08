09f9b9c0695d8d3bdbac5cdfe292b2ccd5e5c59b
angad <angadsharma1016@gmail.com>
2018-06-08 23:34:00 +0530

    procfile

--- app.js
+++ app.js
@@ -69,6 +69,6 @@ app.use("/auth",auth_router);
 
 //listen on specified port
 
-app.listen(process.env.PORT,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Listening on localhost: " + process.env.PORT);
 });
