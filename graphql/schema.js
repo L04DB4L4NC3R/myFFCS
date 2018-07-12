21794ba881f2a510e3f47106a017141be2167e83
angad <angadsharma1016@gmail.com>
2018-07-12 21:39:02 +0530

    faculty search

--- graphql/schema.js
+++ graphql/schema.js
@@ -25,6 +25,22 @@ const CourseType = new GraphQLObjectType({
 
 
 
+const TeacherType = new GraphQLObjectType({
+    name:"Teacher",
+    fields:()=>({
+        VENUE:{type:GraphQLString},
+        CODE:{type:GraphQLString},
+        TITLE:{type:GraphQLString},
+        TYPE:{type:GraphQLString},
+        CREDITS:{type:GraphQLInt},
+        SLOT:{type:GraphQLString},
+        FACULTY:{type:GraphQLString}
+    })
+})
+
+
+
+
 
 const RootQuery = new GraphQLObjectType({
     name:"RootQuery",
@@ -34,7 +50,14 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent,args){
                 return courseModel.find({});
             }
-        }
+        },
+        faculty:{
+            type:new GraphQLList(TeacherType),
+            args:{name:{type:GraphQLString}},
+            resolve(teacher,args){
+                return courseModel.find({FACULTY:args.name})
+            }
+        }   
     })
 });
 
